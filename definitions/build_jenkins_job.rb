b8cfa36b407ffb2e35b5354272ff188215aa223a
Cameron Johnston <cameron@needle.com>
2012-11-23 16:11:41 -0700

    wrapping definition shell out calls in ruby_block resources

--- definitions/build_jenkins_job.rb
+++ definitions/build_jenkins_job.rb
@@ -6,12 +6,20 @@ define :build_jenkins_job, :delete => false, :config_path => '/etc/jenkins_jobs/
   end
 
   unless params[:delete]
-    Chef::Log.info("build_jenkins_job: updating jobs from #{params[:job_config]}")
-    update_job = Chef::ShellOut.new("jenkins-jobs -c #{params[:config_path]} update #{params[:job_config]}").run_command
-    Chef::Log.debug("build_jenkins_job: " + update_job.stderr.inspect)
+    ruby_block "update #{params[:name]} job config" do
+      block do
+        Chef::Log.info("build_jenkins_job: updating jobs from #{params[:job_config]}")
+        update_job = Chef::ShellOut.new("jenkins-jobs -c #{params[:config_path]} update #{params[:job_config]}").run_command
+        Chef::Log.debug("build_jenkins_job: " + update_job.stderr.inspect)
+      end
+    end
   else
-    Chef::Log.info("build_jenkins_job: deleting job #{params[:name]}")
-    delete_job = Chef::ShellOut.new("jenkins-jobs -c #{params[:config_path]} delete #{params[:name]}").run_command
-    Chef::Log.debug("build_jenkins_job: " + delete_job.stderr.inspect)
+    ruby_block "delete #{params[:name]} job config" do
+      block do
+        Chef::Log.info("build_jenkins_job: deleting job #{params[:name]}")
+        delete_job = Chef::ShellOut.new("jenkins-jobs -c #{params[:config_path]} delete #{params[:name]}").run_command
+        Chef::Log.debug("build_jenkins_job: " + delete_job.stderr.inspect)
+      end
+    end
   end
 end
