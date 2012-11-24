d5f8b62778ea23955f6eec25bb08e959f12463c6
Cameron Johnston <cameron@needle.com>
2012-11-23 21:46:10 -0700

    adding checking exit status of shellout commands

--- definitions/build_jenkins_job.rb
+++ definitions/build_jenkins_job.rb
@@ -10,7 +10,11 @@ define :build_jenkins_job, :delete => false, :config_path => '/etc/jenkins_jobs/
       block do
         Chef::Log.info("build_jenkins_job: updating jobs from #{params[:job_config]}")
         update_job = Chef::ShellOut.new("jenkins-jobs --conf #{params[:config_path]} update #{params[:job_config]}").run_command
-        Chef::Log.debug("build_jenkins_job: " + update_job.stderr.inspect)
+        Chef::Log.debug("build_jenkins_job: " + update_job.format_for_exception)
+        unless update_job.status == 0
+          Chef::Log.fatal('build_jenkins_job: error updating job: ' + update_job.format_for_exception)
+          raise
+        end
       end
     end
   else
@@ -18,7 +22,11 @@ define :build_jenkins_job, :delete => false, :config_path => '/etc/jenkins_jobs/
       block do
         Chef::Log.info("build_jenkins_job: deleting job #{params[:name]}")
         delete_job = Chef::ShellOut.new("jenkins-jobs --conf #{params[:config_path]} delete #{params[:name]}").run_command
-        Chef::Log.debug("build_jenkins_job: " + delete_job.stderr.inspect)
+        Chef::Log.debug("build_jenkins_job: " + delete_job.format_for_exception)
+        unless delete_job.status == 0
+          Chef::Log.fatal('build_jenkins_job: error deleting job: ' + delete_job.format_for_exception)
+          raise
+        end
       end
     end
   end
