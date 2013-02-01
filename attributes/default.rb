f631f9de24ac471bc25ec4add3c075c53668dfe5
Cameron Johnston <cameron@needle.com>
2013-02-01 11:37:21 -0700

    defaulting username and password attributes to String.new instead of nil

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['jenkins_job_builder']['user'] = 'jenkins'
 default['jenkins_job_builder']['group'] = 'jenkins'
-default['jenkins_job_builder']['username'] = nil
-default['jenkins_job_builder']['password'] = nil
+default['jenkins_job_builder']['username'] = String.new
+default['jenkins_job_builder']['password'] = String.new
 default['jenkins_job_builder']['url'] = 'http://jenkins.example.com'
