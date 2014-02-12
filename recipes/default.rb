7d52dc630c8e7b0ae58dca9d4c491fbda6be75fe
Patrick Connolly <patrick@myplanetdigital.com>
2014-02-12 16:27:40 -0500

    Removes deprecated external_url config from ini file.

--- recipes/default.rb
+++ recipes/default.rb
@@ -39,7 +39,6 @@ template '/etc/jenkins_jobs/jenkins_jobs.ini' do
     :username => node['jenkins_job_builder']['username'],
     :password => node['jenkins_job_builder']['password'],
     :url => node['jenkins_job_builder']['url'],
-    :external_url => node['jenkins']['server']['url'],
     :hipchat_token => node['jenkins_job_builder']['hipchat_token']
   })
 end
